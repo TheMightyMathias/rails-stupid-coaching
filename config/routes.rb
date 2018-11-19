6c90d7dd698a8a348ba706f0099be62dfa1982ca
Mathias Sellam <mathias.sellam@essec.edu>
2018-11-19 16:51:11 +0100

    program functionning, have to finish display

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'questions/answer', to: 'questions#answer', as: :answer
+  root to: 'questions#ask'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
